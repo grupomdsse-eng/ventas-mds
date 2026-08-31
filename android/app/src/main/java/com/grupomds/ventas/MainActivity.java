cbbf5f9729b368f7980c519829120547399842d4
grupomdsse-eng <grupomdsse@gmail.com>
2026-08-31 16:43:42 +0200

    Add files via upload

--- android/app/src/main/java/com/grupomds/ventas/MainActivity.java
+++ android/app/src/main/java/com/grupomds/ventas/MainActivity.java
@@ -1,5 +1,24 @@
 package com.grupomds.ventas;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Mantiene el WebView dentro de las barras del sistema: evita que el
+        // contenido remoto quede oculto tras la cámara, la hora o la barra inferior.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        getWindow().setStatusBarColor(Color.rgb(6, 44, 95));
+        getWindow().setNavigationBarColor(Color.rgb(4, 29, 67));
+        WindowInsetsControllerCompat bars = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        bars.setAppearanceLightStatusBars(false);
+        bars.setAppearanceLightNavigationBars(false);
+    }
+}
