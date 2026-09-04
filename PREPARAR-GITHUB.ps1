120c45dd962bbf641bd894b77038ad3ef82ca500
grupomdsse-eng <grupomdsse@gmail.com>
2026-09-04 08:37:31 +0200

    Add files via upload

--- PREPARAR-GITHUB.ps1
+++ PREPARAR-GITHUB.ps1
@@ -2,7 +2,7 @@
 # No incluye dependencias, claves, APKs ni instaladores generados.
 $ErrorActionPreference = 'Stop'
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
-$destination = Join-Path (Split-Path -Parent $root) 'MDS-Ventas-Native-v15-GitHub.zip'
+$destination = Join-Path (Split-Path -Parent $root) 'MDS-Ventas-Native-v16-GitHub.zip'
 $temporary = Join-Path ([System.IO.Path]::GetTempPath()) ('mds-ventas-github-' + [guid]::NewGuid().ToString('N'))
 
 function New-PortableZip([string]$source,[string]$target) {
